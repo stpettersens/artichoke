a75e49b4aacb2298a236d692714ee3ce689a2e66
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-06-20 18:47:16 +0100

    ...

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,13 @@ const mocha = require('gulp-mocha')
 const clean = require('gulp-rimraf')
 const wait = require('gulp-wait')
 const sequence = require('gulp-sequence')
+const fs = require('fs')
+
+gulp.task('files', function () {
+  fs.writeFileSync('control.tar.gz', 0)
+  fs.writeFileSync('data.tar.gz', 0)
+  fs.writeFileSync('debian-binary', 0)
+})
 
 gulp.task('test1', function () {
   return gulp.src(['control.tar.gz', 'data.tar.gz', 'debian-binary'])
@@ -19,3 +26,4 @@ gulp.task('test2', function () {
 
 gulp.task('clean', ['test1'])
 gulp.task('test', sequence('test1', 'test2'))
+gulp.task('appveyor_test', sequence('files', 'test'))
