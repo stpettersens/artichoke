58185a3178b62a941ca619b11409a4ff05c8961e
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-06-28 00:04:50 +0100

    pattern testing

--- pattern.cc
+++ pattern.cc
@@ -4,14 +4,17 @@
 using namespace std;
 
 int main() {
-  string hpattern = "\\d{10}"; //([\\w\\-\\\\./]+)\\s*(\\d{10})\\s{2}(\\d{4})\\s{2}(\\d{4})"; //.*";
+  string hpattern = "([\\w\\-\\\\./]+)\\s*(\\d{10})\\s{2}(\\d{4})\\s{2}(\\d{4})"; //.*";
   //hpattern.append("\\s{2}(\\d{6})\\s{2}(\\d{1,4}).*");
 
-  string header = "debian-binary/  123456789  1234  1234  1234  123456  12";
+  string header = "debian-binary/  0123456789  1234  1234  1234  123456  1234";
   regex p(hpattern);
 
-  //smatch m;
-  //bool a = regex_match(header, m, p);
-  //cout << a << endl;
+  smatch m;
+  bool im = regex_search(header, m, p);
+  cout << im << endl;
+  if(regex_search(header, m, p)) {
+    cout << m[0].str() << endl;
+  }
   return 0;
 }
