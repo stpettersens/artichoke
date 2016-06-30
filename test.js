99457111be9007d810871544a20d39eec95a124a
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-06-30 17:09:14 +0100

    ...

--- test.js
+++ test.js
@@ -4,4 +4,4 @@ const artichoke = require('./artichoke')
 
 /* artichoke.createArchive('bar.deb', [ 'debian-binary', 'control.tar.gz', 'data.tar.gz' ],
  {native: true, verbose: true}) */
-artichoke.unpackArchive('demo_0.1-1.deb', {native: false, verbose: true})
+artichoke.unpackArchive('demo_0.1-1.deb', {native: true, verbose: true})
